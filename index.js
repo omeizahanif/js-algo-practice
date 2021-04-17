05b25269f3edf45404d6901c616737d6e211da77
Hanif Adams <omeizahanif@gmail.com>
2021-04-17 13:13:57 +0100

    wrote pig latin algo

---
+++ index.js
@@ -0,0 +1,16 @@
+function pigIt(str){
+    //Code here
+    /*
+    split string into array
+    loop through array,
+    slice first letter of each word,
+    concat first letter with new word and 'ay'
+     */
+    return str.split(' ')
+    .map((word) => `${word.slice(1)}${word[0]}ay`)
+    .join(' ');
+}
+
+
+console.log(pigIt('Pig latin is cool'));
+console.log(pigIt('Hello world !'));
