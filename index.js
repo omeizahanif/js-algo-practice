87c90a81fc318090d3a0718c57d3be701af30f9e
Hanif Adams <omeizahanif@gmail.com>
2021-04-18 13:04:39 +0100

    wrote algo for calc functions

--- index.js
+++ index.js
@@ -14,6 +14,75 @@ function pigIt(str){
     .join(' ');
 }
 
+//calculations using functions
 
-console.log(pigIt('Pig latin is cool'));
-console.log(pigIt('Hello world !'));
+function zero(fn = null) {
+    return fn ? fn(0) : 0
+}
+
+function one(fn = null) {
+    return fn ? fn(1) : 1
+}
+
+function two(fn = null) {
+    return fn ? fn(2) : 2
+}
+
+function three(fn = null) {
+    return fn ? fn(3) : 3
+}
+
+function four(fn = null) {
+    return fn ? fn(4) : 4
+}
+
+function five(fn = null) {
+    return fn ? fn(5) : 5
+}
+
+function six(fn = null) {
+    return fn ? fn(6) : 6
+}
+
+function seven(fn = null) {
+    return fn ? fn(7) : 7
+}
+
+function eight(fn = null) {
+    return fn ? fn(8) : 8
+}
+
+function nine(fn = null) {
+    return fn ? fn(9) : 9
+}
+
+function plus(x) {
+    return function(y) {
+        return y + x;
+    }
+}
+
+function minus(x) {
+    return function(y) {
+        return y - x;
+    }
+}
+
+function times(x) {
+    return function(y) {
+        return y * x;
+    }
+}
+
+function dividedBy(x) {
+    return function(y) {
+        return Math.floor(y / x);
+    }
+}
+
+
+console.log(five());
+console.log(four());
+console.log(
+    eight(dividedBy(four()))
+    );
