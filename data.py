81df53198a1a0d3865147f312b98175ccea6026a
Hanif Adams <omeizahanif@gmail.com>
2021-05-26 12:33:25 +0100

    adde compareYear and compareMonth py

--- data.py
+++ data.py
@@ -79,7 +79,22 @@ def smallest_positive(in_list):
             
     return positive_list[0] or None
 
+def compareYear(year1, month1, day1, year2, month2, day2):
+    if year1 < year2:
+        return True
+    else:
+        compareMonth(month1, day1, month2, day2)
+    return False
 
+def compareMonth(month1, day1, month2, day2):
+    if month1 < month2:
+        return True
+    else:
+        if day1 < day2:
+            return True
+        else:
+            return False
+            
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1
        and year2/month2/day2. Assumes inputs are valid dates
